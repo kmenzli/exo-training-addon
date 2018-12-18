b3c2e2249a3437bbd6ae38c524d69d9f32ee9c03
Khemais Menzli <kmenzli@exoplatform.com>
2018-12-18 12:55:22 +0100

    Init Addon training

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Training></Traiing>
+  </div>
+</template>
+<script>
+import Training from 'components/Training.vue'
+export default {
+  data () {
+    return {
+      message : "Les exoers sont les meilleurs !!!"
+    }
+  }
+}
+</script>
---
+++ src/components/Training.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Training eXo</h1>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message : "Les exoers sont les meilleurs !!!"
+    }
+  }
+}
+</script>
